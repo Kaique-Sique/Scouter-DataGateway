738c6ab8981768ac1d2a2d7dd7849e2a492d7402
Kaique <kaiquedepaula666@gmail.com>
2026-08-16 21:08:46 -0300

    feat: add dotenv dependency for environment variable management

--- src/main/java/com/scouter/gateway/controller/health/HealthDatabaseProvider.java
+++ src/main/java/com/scouter/gateway/controller/health/HealthDatabaseProvider.java
@@ -1,8 +1,12 @@
 package com.scouter.gateway.controller.health;
 
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+/**
+ * HealthDatabaseProvider
+ */
 @Component
 public class HealthDatabaseProvider {
     private final JdbcTemplate jdbcTemplate;
@@ -17,13 +21,17 @@ public class HealthDatabaseProvider {
         long startTime = System.currentTimeMillis();
 
         try {
+            // Query test script
             jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+
             long latency = System.currentTimeMillis() - startTime;
-            return new HealthDependency("Supabase-db", "up", latency);
+
+            return new HealthDependency("Supabase-db", "UP", latency);
 
         } catch (Exception e) {
             long latency = System.currentTimeMillis() - startTime;
             return new HealthDependency("Supabase-db", e.toString(), latency);
+
         }
     }
 }
